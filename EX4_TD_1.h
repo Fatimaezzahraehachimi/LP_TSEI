caeb3cd23bfe4dd486585af4763e88fce8e84fd8
Fatimaezzahraehachimi <94650915+Fatimaezzahraehachimi@users.noreply.github.com>
2021-11-19 11:15:01 +0100

    Create EX4_TD_1.h

---
+++ EX4_TD_1.h
@@ -0,0 +1,8 @@
+int EX4_TD_1(){
+ int a,b;
+ float c;
+ printf("Entrer les 3 valeurs \n");
+ scanf("%d%d%f",&a,&b,&c);
+ printf("\n%d\n%d\n%f",a,b,c);
+ printf("dans la mème ligne\n%d\n%d\n%f",a,b,c);
+}
