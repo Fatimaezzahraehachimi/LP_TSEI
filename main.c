ba579121f1dcd8b47cc8476cee8fb06898366979
Fatimaezzahraehachimi <94650915+Fatimaezzahraehachimi@users.noreply.github.com>
2021-11-19 14:09:48 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "EX1_TD_1.h"
+#include "EX2_TD_1.h"
+#include "EX3_TD_1.h"
+#include "EX4_TD_1.h"
+#include "EX6_TD_1.h"
+#include "EX7_TD_1.h"
+#include "EX8_TD_1.h"
+
+void    main()
+{
+   EX1_TD_1 ();
+   EX2_TD_1();
+   EX3_TD_1();
+   EX4_TD_1();
+   EX6_TD_1();
+   EX7_TD_1();
+   EX8_TD_1();
+    
+
+
+}
+© 2021 GitHub, Inc.
+Terms
